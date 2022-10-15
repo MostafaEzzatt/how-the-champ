8e61121c29baffb4d8bc7909a2c10a68754f0716
Mostafa Ezzat <psmostafa@gmail.com>
2022-10-15 09:26:52 +0200

    add security checks

--- src/pages/api/getChamps.ts
+++ src/pages/api/getChamps.ts
@@ -4,22 +4,26 @@ import { generateRandomNumber } from "../../utils/generateRandomNumber";
 import { readFile } from "../../utils/readFile";
 
 type Data = {
-    ok: Boolean;
+    error?: string;
 };
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    const data = JSON.parse(readFile("champions.json"));
+    const champions = readFile("champions.json") || false;
+    const data = champions ? JSON.parse(champions) : false;
 
-    for (let champ in data) {
-        const champObj = data[champ];
-        const imagesLength = champObj.imagesURLs.length - 1;
-        data[champ].imagesURLs = [
-            data[champ].imagesURLs[generateRandomNumber(0, imagesLength)],
-        ];
+    if (data) {
+        for (let champ in data) {
+            const champObj = data[champ];
+            const imagesLength = champObj.imagesURLs.length - 1;
+            data[champ].imagesURLs = [
+                data[champ].imagesURLs[generateRandomNumber(0, imagesLength)],
+            ];
+        }
+
+        return res.status(200).json(data);
     }
-
-    res.status(200).json(data);
+    return res.status(500).json({ error: "champions cant found" });
 }
