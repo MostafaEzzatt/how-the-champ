485294319cb1a93e4a02ee795c9f35078fc218c6
Mostafa Ezzat <psmostafa@gmail.com>
2022-10-15 11:47:01 +0200

    modify the response of champions trpc route

--- src/server/trpc/router/champions.ts
+++ src/server/trpc/router/champions.ts
@@ -1,10 +1,20 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { generateRandomNumber } from "../../../utils/generateRandomNumber";
+import { generateSplashArtURL } from "../../../utils/getChampionsData";
 import { trpc } from "../../../utils/trpc";
 import { publicProcedure, router } from "../trpc";
 
 export const championsRouter = router({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.champions.findMany();
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const champions = await ctx.prisma.champions.findMany();
+        return champions.map((champ) => ({
+            ...champ,
+            imagesURLs: [
+                champ.imagesURLs[
+                    generateRandomNumber(0, champ.imagesURLs.length - 1)
+                ],
+            ],
+        }));
     }),
 });
