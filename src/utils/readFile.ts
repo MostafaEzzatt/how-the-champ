74c89fd1fa485edac807f8f96ed2665803f265d4
Mostafa Ezzat <psmostafa@gmail.com>
2022-10-15 11:59:14 +0200

    delete unused functions

--- src/utils/readFile.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-
-export const readFile = (fileName: string) => {
-    if (fs.existsSync(`data/${fileName}`)) {
-        return fs.readFileSync(`data/${fileName}`, "utf8");
-    }
-
-    return null;
-};
--- src/utils/saveFile.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs";
-
-export const saveFile = (data: Object, fileName: string) => {
-    if (!fs.existsSync("data")) {
-        fs.mkdirSync("data");
-    }
-    fs.writeFileSync(`data/${fileName}`, JSON.stringify(data, null, 4));
-};
